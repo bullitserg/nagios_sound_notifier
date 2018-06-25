cfd432207ed76a9cc2c2c0790c361c23d52a0fee
belims <bullit88@mail.ru>
2018-06-25 12:01:59 +0300

    Правка конфига

--- config.dist.py
+++ config.dist.py
@@ -1,7 +1,7 @@
 nagios_web_page = 'http://nagios-m1.etp-micex.ru/nagios/cgi-bin/status.cgi?hostgroup=all&style=detail&servicestatustypes=28&hoststatustypes=15'
 nagios_local_page = 'C:/Users/belim/PycharmProjects/Nagios sound notifier/nagios.html'
-nagios_login = 'monitoring'
-nagios_password = 'monitoring'
+nagios_login = '****'
+nagios_password = '****'
 nagios_page_classes = ['statusBGCRITICAL', 'statusBGWARNING']
 
 metric_source = 'C:/Users/belim/PycharmProjects/Nagios sound notifier/metrics.source'
